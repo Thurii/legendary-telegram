61e80c574c3fec5231e75c0d1fe54fbb61bb5e86
Thurii <sciencegeek1999@gmail.com>
2016-05-27 10:57:16 -0500

    Revamped status effects

--- test_enemy.py
+++ test_enemy.py
@@ -1,10 +1,10 @@
 from random import randint
 from element_game_stats import testStats
 
-def test(opponentHP,opponentEP,opponentPoisoned,opponentSinkHole,opponentDisarmed):
+def test(opponentHP,opponentEP,statusEffects):
 	damage = 0
 
-	if opponentPoisoned == True:
+	if statusEffects['opponentPoisoned'] == True:
 		opponentHP = opponentHP - 5
 
 	if randint(0,100) <= testStats['epRegenChance']:
@@ -12,12 +12,12 @@ def test(opponentHP,opponentEP,opponentPoisoned,opponentSinkHole,opponentDisarme
 
 	opponentAttack = randint(1,4)
 
-	if opponentDisarmed > 0:
+	if statusEffects['opponentDisarmed'] > 0:
 		if opponentDisarmed == 2:
 			print('Test\'s wand leaps out of his hand!')
 		else:
 			print('Test fumbles for his wand...')
-		opponentDisarmed = opponentDisarmed - 1
+		statusEffects['opponentDisarmed'] = statusEffects['opponentDisarmed'] - 1
 
 	elif opponentAttack == 1:
 		if opponentEP >= 25:
@@ -51,12 +51,12 @@ def test(opponentHP,opponentEP,opponentPoisoned,opponentSinkHole,opponentDisarme
 		else:
 			print('Test looked around for a bandaid...')
 
-	if opponentSinkHole > 0:
+	if statusEffects['opponentSinkHole'] > 0:
 		opponentEP = opponentEP - 25
-		opponentSinkHole = opponentSinkHole - 1
-		if opponentSinkHole == 0:
+		statusEffects['opponentSinkHole'] = statusEffects['opponentSinkHole'] - 1
+		if statusEffects['opponentSinkHole'] == 0:
 			print('Test got out of the Sink Hole!')
 
 	input()
 
-	return dict(zip(['opponentHP','opponentEP','damage','opponentPoisoned','opponentSinkHole','opponentDisarmed'],[opponentHP,opponentEP,damage,opponentPoisoned,opponentSinkHole,opponentDisarmed]))
+	return dict(zip(['opponentHP','opponentEP','damage','statusEffects'],[opponentHP,opponentEP,damage,statusEffects]))
